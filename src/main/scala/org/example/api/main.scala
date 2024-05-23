48ece3c0cc05f177f3acf31e122a1db63ee87a55
apple-corps <>
2024-05-22 17:21:36 -0700

    add mutations

--- src/main/scala/org/example/api/main.scala
+++ src/main/scala/org/example/api/main.scala
@@ -7,12 +7,17 @@ import caliban.RootResolver
 import org.example.api._
 import org.example.api.Types._ 
 import org.example.api.Operations._
-import org.example.api.Queries._
 
 object Resolvers {
   var users: Map[Int, User] = Map()
 
     val queryResolver = Query(
-        getUser = args => ZIO.succeed(users.get(args.input.systemID.getOrElse(0)))
+      getUser = args => ZIO.succeed(users.get(args.input.systemID.getOrElse(0)))
+    )
+
+    val mutationResolver = Mutation(
+      createUser = args /*: MutationCreateUserArgs */ => ZIO.succeed(None),
+      updateUser = args => ZIO.succeed(None),
+      deleteUser = args => ZIO.succeed(true)
     )
 }
