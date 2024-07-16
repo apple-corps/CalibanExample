87ab095fa0e5b421a0d7976305eb6af071ac2a0e
apple-corps <>
2024-07-16 13:10:00 -0700

    partially functional

--- src/main/scala/org/example/quill/QuillTestApp.scala
+++ src/main/scala/org/example/quill/QuillTestApp.scala
@@ -1,28 +1,35 @@
 package org.example.quill
+
 import io.getquill.*
 import org.example.api.Types.*
 import com.typesafe.config.ConfigFactory
 import io.getquill.context.qzio.ImplicitSyntax._
 import io.getquill.util.LoadConfig
 import io.getquill.autoQuote
+import zio.{ZIO, ZLayer, ZIOAppDefault}
 
 import javax.sql.DataSource
 
-object QuillTestApp {
+object QuillTestApp extends ZIOAppDefault {
   object Ctx extends PostgresZioJdbcContext(Literal)
-  import Ctx._
+  import Ctx.*
+
   lazy val ds = JdbcContextConfig(LoadConfig("link")).dataSource
   given Implicit[DataSource] = Implicit(ds)
 
-  def main(args: Array[String]): Unit = {
+  override def run = {
     val named = "discord"
 
     inline def somePeople = quote {
       query[User].filter(u => u.fullName == lift(named))
     }
 
-    val people : List[User] = run(somePeople)
-    // TODO Get SQL
-    println(people)
-    }
+    val peopleEffect: ZIO[DataSource, Throwable, List[User]] = Ctx.run(somePeople)
+
+    peopleEffect.provide(ZLayer.succeed(ds)) // Provide the DataSource
+      .tap { people =>
+        ZIO.attempt(println(s"People: $people"))
+      }
+      .exitCode
+  }
 }
